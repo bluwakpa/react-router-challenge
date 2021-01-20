660047e3064445e9cfaceab0f93bde2f356b0e05
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-01-20 15:26:12 -0800

    init

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import AboutMenu from './about-menu';
 
 export default function App(props) {
