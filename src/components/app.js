2555670eac026fc744d825c34f26fa61baa8cede
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-01-20 17:04:33 -0800

    functional

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
 import AboutMenu from './about-menu';
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Route exact path='/about/foo' component={AboutMenu} />
+            <Route exact path='/about/bar' component={AboutMenu} />
+            <Route exact path='/about/foo/bar' component={AboutMenu} />
         </div>
     );
 }
